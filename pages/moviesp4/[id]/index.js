418ad659a79cf03cdc178056d982c4d47b4bd37f
mxplayermovies <46235644+RonaldDsouza@users.noreply.github.com>
2024-08-28 23:08:32 +0530

    ok  donation

--- pages/moviesp4/[id]/index.js
+++ pages/moviesp4/[id]/index.js
@@ -322,30 +322,7 @@ const moviesDetail4 = ({ moviesItem }) => {
   const prevEpisodeNumber = episode - 1 < 1 ? videoSources.length : episode - 1
   const nextEpisodeNumber = (episode % videoSources.length) + 1
 
-  useEffect(() => {
-    // Add the Ko-fi widget script to the page
-    const script = document.createElement('script');
-    script.src = 'https://storage.ko-fi.com/cdn/scripts/overlay-widget.js';
-    script.async = true;
 
-    script.onload = () => {
-      if (typeof kofiWidgetOverlay !== 'undefined') {
-        kofiWidgetOverlay.draw('payat', {
-          'type': 'floating-chat',
-          'floating-chat.donateButton.text': 'Support me',
-          'floating-chat.donateButton.background-color': '#00b9fe',
-          'floating-chat.donateButton.text-color': '#fff'
-        });
-      }
-    };
-
-    document.body.appendChild(script);
-
-    return () => {
-      // Cleanup if the component is unmounted
-      document.body.removeChild(script);
-    };
-  }, []);
 
   const uwatchfreeSchema = JSON.stringify([
     {
